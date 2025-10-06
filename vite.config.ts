29af5111fbeabe037b3fab8d4957a2de4aa6c5eb
Dmitriy1990 <nickdima13384@gmail.com>
2025-10-06 13:32:22 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import svgr from 'vite-plugin-svgr';
 import paths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  build: {
-    outDir: './build',
-  },
   plugins: [react(), paths(), svgr()],
 });
